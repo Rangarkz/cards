2db758128961b907f06062826b584fb5d2beeea6
750584 <srr66166@gmail.com>
2025-01-25 20:23:20 +0530

    creatd Post interface and created posts data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./card/card.component";
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,23 @@ import { CardComponent } from "./card/card.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cards';
+  posts: Post[] = [{
+    title: "Neat tree",
+    username: "nature",
+    image: "assets/tree.jpeg",
+    content: "I saw this neat tree today"
+  },
+  {
+    title: "Snowy mountain",
+    username: "mountainlover",
+    image: "assets/mountain.jpeg",
+    content: "Here is a picture of a snowy mountain"
+  },
+  {
+    title: "Mountain Biking",
+    username: "biking12222",
+    image: "assets/biking.jpeg",
+    content: "I did some biking today"
+  }
+  ];
 }
---
+++ src/app/post.ts
@@ -0,0 +1,6 @@
+export interface Post {
+    title: string;
+    username: string;
+    image: string;
+    content: string;
+}
